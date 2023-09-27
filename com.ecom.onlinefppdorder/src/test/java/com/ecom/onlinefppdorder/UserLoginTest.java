558a08c3b6573f0fad4b162ae8a5a789778e45b3
dmoni <dmoni@DHAMONI>
2023-09-27 17:46:55 +0530

    User login created

---
+++ com.ecom.onlinefppdorder/src/test/java/com/ecom/onlinefppdorder/UserLoginTest.java
@@ -0,0 +1,11 @@
+package com.ecom.onlinefppdorder;
+
+import org.testng.annotations.Test;
+
+public class UserLoginTest {
+@Test
+public void userLogin()
+{
+	System.out.println("---Test script 1----");
+}
+}
